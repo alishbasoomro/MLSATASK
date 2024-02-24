8a886f0c889e4e269cf784579c550edf68ed6e04
Alishba <alishba.soomro@student.ambassador.com>
2024-02-25 00:59:31 +0500

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "girl-2618562_1280.jpg";
+import image from "paint-2985569_1280.jpg";
 
-const imageAltText = " Girl sitting on a chair doing something.";
+const imageAltText = "Creative painting.";
 
 const Home = ({ name, title }) => {
   return (
