0918981e2068cc8764f39c4641922e28e086a2dc
Alishba <alishba.soomro@student.ambassador.com>
2024-02-25 02:37:42 +0500

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "office.jpeg";
+import image from "../office.jpeg";
 
 const imageAltText = "desktop with books, glasses and a cup of coffee.";
 
